91f2b6547e5b34247a10e472d7371de3c4d2295b
Ismail Azdad <ismail.azdad@adventori.com>
2022-03-21 17:01:24 +0100

    clean preprocessing pattern

--- controller.py
+++ controller.py
@@ -1,12 +1,25 @@
 import sys
 from flask import Flask, jsonify, request, render_template, Config
-import torch
-from PIL import Image
+import pandas as pd
+import joblib
+import spacy
+import en_core_web_sm
+
 import datetime
 from datetime import datetime
+import os
+
+# from .preprocessing import Preprocessing
+
+# from test.preprocessing import Preprocessing
+
+from service.preprocessing import Preprocessing
+
+
+
 
 app = Flask(__name__)
-
+print(os.getcwd())
 
 @app.route('/')
 def index():
@@ -17,7 +30,16 @@ def index():
 def hello():
     return render_template('index.html', utc_dt=datetime.utcnow())
 
-
+@app.route('/tagGenerators')
+def tagGenerators():
+    # question  = request.form.get("question")
+    # print(question)
+    question = 'java html css'
+    print('passage controller')
+    t =  Preprocessing('java html css')
+    cleaned_question =  t.text_cleaner(question)
+    return 'test'
+# Cleaning function for new question
 
 if __name__ == "__main__":
     app.config['env'] = sys.argv[1]
